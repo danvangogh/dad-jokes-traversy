7b86114bbb8a751a6bfdf4e8328957f08c23a644
danvangogh <djameswhite@yahoo.com>
2019-05-29 06:32:11 -0700

    displays joke list

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,12 +1,51 @@
 <template>
   <div>
-    jokes
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"/>
   </div>
 </template>
 
 <script>
-  export default {
 
+  import axios from 'axios';
+  import Joke from '../../components/Joke';
+
+  export default {
+    components: {
+      Joke
+    },
+    data() {
+      return {
+        jokes: []
+      }
+    },
+    async created() {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+
+      try {
+        const res = await axios.get('https://icanhazdadjoke.com/search', config);
+        this.jokes = res.data.results;
+        console.log("this.jokes @ try:", res.data.results)
+      } catch {
+        console.log(err)
+      }
+
+    },
+    head() {
+      return {
+        title: 'Jokes',
+        meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for bad jokes'
+        }
+        ]
+      }
+    }
   }
 </script>
 

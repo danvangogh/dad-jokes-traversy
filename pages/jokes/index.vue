5fb6df97ba286dc2305b33d2344bc7b7baa39d45
danvangogh <djameswhite@yahoo.com>
2019-05-31 06:44:01 -0700

    dynamic routing

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -24,15 +24,12 @@
           Accept: "application/json"
         }
       };
-
       try {
         const res = await axios.get('https://icanhazdadjoke.com/search', config);
         this.jokes = res.data.results;
-        console.log("this.jokes @ try:", res.data.results)
-      } catch {
+      } catch (err) {
         console.log(err)
       }
-
     },
     head() {
       return {
